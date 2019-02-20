9ae31b8281f0c8451d87180a8e6cd4ad266ec79e
kino002 <ads11468@gmail.com>
2019-02-20 09:58:53 +0900

    spi_slave追加

--- spi_slave/spi_slave.ino
+++ spi_slave/spi_slave.ino
@@ -4,19 +4,23 @@ void setup (void) {
   pinMode(MISO, OUTPUT);
   Serial.begin(9600);
  
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE1 );
-  SPCR |= _BV(SPE);
+  SPI.setBitOrder(MSBFIRST);    // 入出力のビットオーダー設定
+  SPI.setDataMode(SPI_MODE1 );  // 通信モード設定
+  SPCR |= _BV(SPE);             // SPI Enable
   pinMode(SS, INPUT);
-  SPI.attachInterrupt();
+  SPI.attachInterrupt();  // SPI割り込み開始
 }
  
 // SPI割り込み
 ISR (SPI_STC_vect) {
+  // SPDRレジスタを入れ物としてデータの送受信を行う
   byte cc = SPDR; //SPDRレジスタにデータが送られてくる
-  cc++;
-  SPDR = cc;//SPDRレジスタにあるデータが送信される
-  Serial.println(cc);
+  if(cc == 78){
+    // 処理を書く
+    Serial.println("Hello World");
+  }
+  // ここでSPDRに書き込まなければ、何も送信されない。
+  SPDR = 55;//SPDRレジスタにあるデータが送信される
 }
  
 void loop (void) {
